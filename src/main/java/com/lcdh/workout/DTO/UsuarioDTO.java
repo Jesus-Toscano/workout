053d07866463930af7c5f6540213c66ee1c72589
Jesus-Toscano <jesus.toscano.tlsir@gmail.com>
2024-04-21 09:22:57 -0600

    agregue swagger

--- src/main/java/com/lcdh/workout/DTO/UsuarioDTO.java
+++ src/main/java/com/lcdh/workout/DTO/UsuarioDTO.java
@@ -1,5 +1,7 @@
 package com.lcdh.workout.DTO;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotEmpty;
 import lombok.*;
 
 import java.math.BigDecimal;
@@ -12,10 +14,13 @@ public class UsuarioDTO  {
 
 
     private Long id;
+    @NotEmpty(message = "Registra un nombre")
     private String nombre;
+    @NotEmpty(message = "Registra un apellido")
     private String apellido;
+    @NotEmpty(message = "Registra una edad")
     private String edad;
-    private BigDecimal tiempoCarrera;
+    private Double tiempoCarrera;
     private int dominadas;
     private int lagartijas;
 
